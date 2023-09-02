6b5feccb1339dd7bdfa867bf50b5367d9fd44c39
Carlos Pires <extraterra1@gmail.com>
2023-09-02 16:59:55 +0100

    feat: add UI for RPS game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function playRound(playerSelection, computerSelection) {
-  switch (playerSelection) {
+function evaluateRound(playerSelection, computerSelection) {
+  switch (playerSelection.toLowerCase()) {
     case "paper": {
       if (computerSelection == "rock") return "You win! Paper beats Rock";
       if (computerSelection == "scissors") return "You lose! Scissors beats Paper";
@@ -26,22 +26,42 @@ const getComputerChoice = () => {
   return choices[randomN];
 };
 
-const game = () => {
-  let roundsPlayed = 0;
-  let playerScore = 0;
-  let cpuScore = 0;
+// console.log(game());
+let roundsPlayed = 0;
+let playerScore = 0;
+let cpuScore = 0;
 
-  while (roundsPlayed < 5) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt(`Round ${roundsPlayed + 1}! PLAY`).toLowerCase();
-    const result = playRound(computerSelection, playerSelection);
-    if (result.includes("win")) ++playerScore;
-    if (result.includes("lose")) ++cpuScore;
-    console.log(result);
-    ++roundsPlayed;
-  }
-
-  return `Final Score: PLAYER ${playerScore} - ${cpuScore} CPU`;
+const handleSelection = (e) => {
+  buttons.forEach((el) => el.classList.remove("selected"));
+  e.target.classList.toggle("selected");
+  playRound(e);
 };
 
-console.log(game());
+const playRound = (e) => {
+  let playerSelection = document.querySelector("span.selected");
+  if (!playerSelection) return (document.querySelector(".result h1").textContent = "You need to select something");
+  playerSelection = playerSelection.getAttribute("data-item");
+  const computerSelection = getComputerChoice();
+  const result = evaluateRound(playerSelection, computerSelection);
+
+  if (result.includes("win")) ++playerScore;
+  if (result.includes("lose")) ++cpuScore;
+
+  document.querySelector(".playerScore").textContent = playerScore;
+  document.querySelector(".cpuScore").textContent = cpuScore;
+
+  if (playerScore > 4 || cpuScore > 4) {
+    document.querySelector(".result h1").textContent = `Final Score ${playerScore - cpuScore > 0 ? "PLAYER WINS" : "CPU WINS"}`;
+    playerScore = 0;
+    cpuScore = 0;
+  } else {
+    document.querySelector(".result h1").textContent = result;
+  }
+};
+
+const buttons = document.querySelectorAll("span.option");
+buttons.forEach((el) => {
+  el.addEventListener("click", handleSelection);
+});
+
+const playButton = document.querySelector("button.play").addEventListener("click", playRound);
